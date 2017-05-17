b63085338e1a273e65c2d266d08b503538bfddb1
Ryan Burn <ryan.burn@gmail.com>
2017-05-17 19:07:07 -0400

    Support opentracing ESI requests.

---
+++ opentracing/src/opentracing_request_context.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <lightstep/tracer.h>
+
+extern "C" {
+#include <varnish/vrt.h>
+}
+
+//------------------------------------------------------------------------------
+// OpenTracingRequestContext
+//------------------------------------------------------------------------------
+struct OpenTracingRequestContext {
+  lightstep::Span span;
+  std::string operation_name;
+};
+
+//------------------------------------------------------------------------------
+// finalize_opentracing_request_context
+//------------------------------------------------------------------------------
+void finalize_opentracing_request_context(void* context);
+
+//------------------------------------------------------------------------------
+// pass_opentracing_request_context_through_header
+//------------------------------------------------------------------------------
+void pass_opentracing_request_context_through_header(
+    const vrt_ctx* ctx, gethdr_e where,
+    OpenTracingRequestContext* tracing_context);
+
+//------------------------------------------------------------------------------
+// receive_opentracing_request_context_from_header
+//------------------------------------------------------------------------------
+OpenTracingRequestContext* receive_opentracing_request_context_from_header(
+    const vrt_ctx* ctx, gethdr_e where);
--- opentracing/src/trace.cpp
+++ opentracing/src/trace.cpp
@@ -1,12 +1,10 @@
 #include <lightstep/tracer.h>
 #include <iostream>
 #include <cinttypes>
-#include "span_map.h"
+#include <cassert>
+#include "opentracing_request_context.h"
 
 extern "C" {
-#include <varnish/vcl.h>
-#include <varnish/vdef.h>
-#include <varnish/vrt.h>
 #include <varnish/vrt_obj.h>
 }
 
@@ -14,78 +12,66 @@ void inject_span_context(lightstep::Tracer& tracer, const vrt_ctx* ctx,
                          gethdr_e where,
                          const lightstep::SpanContext& span_context);
 
-static SpanMap& get_span_map() {
-  static SpanMap span_map;
-  return span_map;
-}
-
-struct TopRequestTracingContext {
-  const vrt_ctx* ctx;
-  struct sess* sp;
-  lightstep::Span span;
-};
-
-static void finish_top_request(void* tracing_context_) {
-  std::cout << "finishing request span...\n";
-  auto tracing_context =
-      static_cast<TopRequestTracingContext*>(tracing_context_);
-  std::cout << "finish: sp = " << tracing_context->ctx->sp << "\n";
-  get_span_map().erase(tracing_context->sp);
-  // How do I figure out if the request resulted in an error or not? Running 
-  // something like
-  //      VRT_r_obj_status(tracing_context->ctx)
-  // won't work because many of the varnish request's resources have already
-  // been cleaned up.
-  tracing_context->span.Finish();
-  delete tracing_context;
-}
-
-static const char* const kOpenTracingParentHeader = "\022ot-varnish-parent:";
-
-static void set_context_header(const vrt_ctx *ctx, const void *ptr) {
-  const size_t max_chars = sizeof(uintptr_t) * 2 + 1;
-  char s[max_chars];
-  snprintf(s, max_chars, "%" PRIxPTR, reinterpret_cast<uintptr_t>(ptr));
-  gethdr_s gethdr = {HDR_REQ, kOpenTracingParentHeader};
-  VRT_SetHdr(ctx, &gethdr, s, vrt_magic_string_end);
-}
-
-static void get_context_header(const vrt_ctx* ctx) {
-  gethdr_s gethdr = {HDR_BEREQ, kOpenTracingParentHeader};
-  auto result = VRT_GetHdr(ctx, &gethdr);
-  if (!result)
-    return;
-  std::cout << "get_context_header: ctx value = " << result << "\n";
-}
-
+//------------------------------------------------------------------------------
+// vmod_trace_request
+//------------------------------------------------------------------------------
 extern "C" VCL_VOID vmod_trace_request(VRT_CTX,
-                                       struct vmod_priv* request_context,
-                                       struct vmod_priv* top_request_context) {
-  if (ctx->http_req != ctx->http_req_top) {
-    std::cout << "tracing ESI requests isn't supported\n";
-    return;
-  }
+                                       struct vmod_priv* request_priv,
+                                       struct vmod_priv* top_request_priv) {
   std::cout << "tracing request ...\n";
-  std::cout << "req: sp = " << ctx->sp << "\n";
   auto tracer = lightstep::Tracer::Global();
-  auto tracing_context = new TopRequestTracingContext{};
-  tracing_context->span = tracer.StartSpan(VRT_r_req_url(ctx));
+  auto tracing_context = new OpenTracingRequestContext{};
+
+  tracing_context->operation_name = VRT_r_req_url(ctx);
+  // Check to see if this is a request generated from an ESI. If so, specify the
+  // top-level request's span as the parent if present.
+  assert(!request_priv->priv && !request_priv->free);
+  if (ctx->http_req != ctx->http_req_top && top_request_priv->priv) {
+    auto parent_span_context =
+        reinterpret_cast<OpenTracingRequestContext*>(top_request_priv->priv)
+            ->span.context();
+    tracing_context->span = tracer.StartSpan(
+        tracing_context->operation_name,
+        {lightstep::SpanReference{lightstep::ChildOfRef, parent_span_context}});
+  } else {
+    tracing_context->span = tracer.StartSpan(tracing_context->operation_name);
+  }
+
   tracing_context->span.SetTag("component", "varnish");
-  tracing_context->ctx = ctx;
-  tracing_context->sp = ctx->sp;
-  top_request_context->priv = tracing_context;
-  top_request_context->free = finish_top_request;
-  get_span_map().insert(ctx->sp, &tracing_context->span);
-  set_context_header(ctx, tracing_context);
+  request_priv->priv = tracing_context;
+  request_priv->free = finalize_opentracing_request_context;
+  if (ctx->http_req == ctx->http_req_top) {
+    assert(!top_request_priv->priv && !top_request_priv->free);
+    top_request_priv->priv = tracing_context;
+  }
+  pass_opentracing_request_context_through_header(ctx, HDR_REQ, tracing_context);
 }
 
-extern "C" VCL_VOID vmod_trace_backend_request(VRT_CTX) {
+//------------------------------------------------------------------------------
+// vmod_trace_backend_request
+//------------------------------------------------------------------------------
+extern "C" VCL_VOID vmod_trace_backend_request(VRT_CTX,
+                                               struct vmod_priv* request_priv) {
   std::cout << "tracing backend request ...\n";
-  std::cout << "fetch: ctx->sp = " << ctx->sp << "\n";
-  get_context_header(ctx);
-  auto parent_span = get_span_map().lookup_span(ctx->sp);
-  if (!parent_span)
-    return;
   auto tracer = lightstep::Tracer::Global();
-  inject_span_context(tracer, ctx, HDR_BEREQ, parent_span->context());
+  auto parent_tracing_context =
+    receive_opentracing_request_context_from_header(ctx, HDR_BEREQ);
+
+  auto tracing_context = new OpenTracingRequestContext{};
+
+  tracing_context->operation_name =
+      "backend:" + parent_tracing_context->operation_name;
+  if (parent_tracing_context) {
+    auto parent_span_context = parent_tracing_context->span.context();
+    tracing_context->span = tracer.StartSpan(
+        tracing_context->operation_name,
+        {lightstep::SpanReference{lightstep::ChildOfRef, parent_span_context}});
+  } else {
+    tracing_context->span = tracer.StartSpan(tracing_context->operation_name);
+  }
+
+  tracing_context->span.SetTag("component", "varnish");
+  request_priv->priv = tracing_context;
+  request_priv->free = finalize_opentracing_request_context;
+  inject_span_context(tracer, ctx, HDR_BEREQ, tracing_context->span.context());
 }
